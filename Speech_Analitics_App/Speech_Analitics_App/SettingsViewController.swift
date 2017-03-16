309d0cbbd778b16c78ec51ef5cdfa54092f64ca7
eddypuffs <nunez087@umn.edu>
2017-03-16 12:11:56 +0000

    First commit. Contains main menu screen with minimal layout

---
+++ Speech_Analitics_App/Speech_Analitics_App/SettingsViewController.swift
@@ -0,0 +1,27 @@
+//
+//  SettingsViewController.swift
+//  Speech_Analitics_App
+//
+//  Created by Eduardo Nunez on 3/5/17.
+//  Copyright © 2017 Eduardo Nunez. All rights reserved.
+//
+
+import Foundation
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
+
